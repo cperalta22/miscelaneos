69aaf7112c5f4b52679c4d50239b6ee0212f1b66
cpa@bbq <cperalta@ifc.unam.mx>
2025-01-19 10:45:42 -0600

    ejemplo texto de color en bash

---
+++ texto_color.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+# Colores para texto
+rojo=`tput setaf 1`
+verde=`tput setaf 2`
+amarillo=`tput setaf 3`
+cyan=`tput setaf 6`
+bkrojo=`tput setab 1`
+reset=`tput sgr0`
+
+echo "${bkrojo}${rojo}:::::::::::::::::::::::::::::::::${reset}${cyan}-RECUERDA-${reset}${bkrojo}${rojo}:::::::::::::::::::::::::::::::::${reset}"
+echo "    ${rojo}:Este es un servidor de análisis compartido, de modo que:${reset}"
+echo "         ${verde}1. Evita dar acceso a otras personas sin el permiso del adminstrador${reset}"
+echo "         ${cyan}2. Usa exclusivamente tu directorio home y mantenlo limpio ${reset}"
+echo "         ${verde}3. Haz respaldos frecuentes ${reset}"
+echo "         ${cyan}4. Se racional con la carga de trabajo ${reset}"
+echo "         ${verde}5. Comunícate con el administrador si lo necesitas"  
+echo "${bkrojo}${rojo}::::::::::::::::${reset}${cyan}- apoot@ifc.unam.mx ó cperalta@ifc.unam.mx -${reset}${bkrojo}${rojo}::::::::::::::::${reset}"
